5b407e67fed4e43771eb314270d354d6b8986971
Niclas Jareborg <niclas@jareborg.se>
2013-09-16 11:57:45 +0200

    Improved interface to getCouchDbData.php

--- getCouchDbData.php
+++ getCouchDbData.php
@@ -1,12 +1,30 @@
 <?php
-    $viewStr = "KPI_applications";
+    //$viewStr = "KPI_applications";
+    $viewStr = "";
+    $designStr = "";
+    $dbStr = "";
+    //$levelStr = "";
+    
     //$group_levelStr = "group_level=2";
     if (isset($_GET['view'])) {
         $viewStr = $_GET['view'];
         //echo "viewstr: $viewStr<br>";
-    } else {
-        $viewStr .= "?group_level=2";
+    //} else {
+    //    $viewStr .= "?group_level=2";
     }
+    if(isset($_GET['design'])) {
+        $designStr = $_GET['design'];
+    }
+    if(isset($_GET['db'])) {
+        $dbStr = $_GET['db'];
+    }
+    if(isset($_GET['reduce'])) {
+        $reduceStr = $_GET['reduce'];
+    }
+    if(isset($_GET['group_level'])) {
+        $levelStr = $_GET['group_level'];
+    }
+    
 
     //$url = "http://localhost:5984/analysis/_design/process_flow/_view/KPI?group_level=2";
     //$url = "http://tools.scilifelab.se:5984/analysis/_design/process_flow/_view/KPI?group_level=2";
@@ -17,9 +35,18 @@
     fclose($fhandle);
     
     // set up curl and call couchDb view
-    $base_url = "http://$user_password@tools.scilifelab.se:5984/projects/_design/process_flow/_view/";
-    //$base_url = "http://localhost:5984/projects/_design/kpi_external/_view/";
-    $url = "$base_url$viewStr";
+
+    $base_url = "http://$user_password@tools.scilifelab.se:5984";
+    //$base_url = "http://localhost:5984";
+    ////"projects/_design/kpi_external/_view/";
+
+    $url = "$base_url/$dbStr/_design/$designStr/_view/$viewStr";
+    if(isset($reduceStr)) {
+        $url .= "?reduce=$reduceStr";
+    }
+    if(isset($levelStr)) {
+        $url .= "?group_level=$levelStr";
+    }
     //echo "url: $url<br>";
     $ch = curl_init($url);
     
