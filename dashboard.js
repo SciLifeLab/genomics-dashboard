ed715620e550926386dc15eca18313a2a096d739
Niclas Jareborg <niclas@jareborg.se>
2013-09-30 10:30:00 +0200

    Changes to make plot sizes dependent on window size

--- dashboard.js
+++ dashboard.js
@@ -548,8 +548,8 @@ function drawRunChart(dataset, divID, clines, width, height, padding, maxY) {
  * @param plotHeight plot height
  */
 function drawBoxPlot(dataset, divID, plotHeight, maxY, bottom_margin) {
-    var margin = {top: 30, right: 50, bottom: 30, left: 50},
-        width = 120 - margin.left - margin.right,
+    var margin = {top: 30, right: 20, bottom: 30, left: 20},
+        width = 60 - margin.left - margin.right,
         //height = 450 - margin.top - margin.bottom;
         //height = 400 - margin.top - margin.bottom;
         height = plotHeight - margin.top - margin.bottom;
@@ -766,8 +766,10 @@ function drawProcessPanels(appl_json, pf_json, plotDate, startDate, height, rc_w
     
     var maxY = Math.max(maxD, maxO);
     
-    drawBarchartPlot(demandDataset, "demand_bc", 500, height, 30, maxY);
-    drawBarchartPlot(ongoingDataset, "ongoing_bc", 500, height, 30, maxY);
+    //drawBarchartPlot(demandDataset, "demand_bc", 500, height, 30, maxY);
+    //drawBarchartPlot(ongoingDataset, "ongoing_bc", 500, height, 30, maxY);
+    drawBarchartPlot(demandDataset, "demand_bc", (rc_width + 110), height, 30, maxY);
+    drawBarchartPlot(ongoingDataset, "ongoing_bc", (rc_width + 110), height, 30, maxY);
     
     //console.log(pf_json);
     var totalRcDataset = generateRunchartDataset(appl_json, startDate, plotDate, startKey, endKey);
