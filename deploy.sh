bd2fd227fb6b1b65b69187e4fac6ca36575ddf6a
Niclas Jareborg <niclas@jareborg.se>
2013-10-29 11:00:17 +0100

    Added resetting of umask after git pull

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+## to make all affected files group writable on git pull,
+## so that group members can update repository
+## without affecting write access for other group members
+
+mask=`umask`
 umask 0007
 git pull
+umask $mask
