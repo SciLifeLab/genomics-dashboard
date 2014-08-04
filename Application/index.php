10cb5dc5b47cbe67be6d24f4575b160f1a767d3e
Denis Moreno <denismoreno@denismmbp.scilifelab.se>
2014-08-04 12:37:05 +0200

    fixed relocate to be generic

--- Application/index.php
+++ Application/index.php
@@ -1,5 +1,6 @@
 <?php
-$location="http://".$_SERVER['HTTP_HOST']."/dashboard_all.php?ptype=Application";
+$appname=explode('/', $_SERVER['REQUEST_URI']);
+$location="http://".$_SERVER['HTTP_HOST']."/".$appname[1]."/dashboard_all.php?ptype=Application";
 header("Location: ".$location);
 
 ?>
